840b63a626c8aef2fd8bdc0f651d7187f8a19437
dn.kalabin <dn.kalabin@tensor.ru>
2024-06-27 19:54:45 +0300

    iter1 complete

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,22 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+
+	"github.com/leary1337/url-shortener/internal/app"
+)
+
+const ServerAddress = `localhost:8080`
+
+func main() {
+	serverHandler := app.NewServerHandler(ServerAddress)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc(`/`, serverHandler.MainPage)
+
+	err := http.ListenAndServe(ServerAddress, mux)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/leary1337/url-shortener
+
+go 1.21.8
