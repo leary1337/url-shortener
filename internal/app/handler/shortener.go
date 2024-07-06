16785adf865f9f97fe94f56cd4bcdf3e1e02e292
dn.kalabin <dn.kalabin@tensor.ru>
2024-07-06 14:15:05 +0300

    iter7 complete

--- internal/app/handler/shortener.go
+++ internal/app/handler/shortener.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
@@ -8,6 +9,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 
+	"github.com/leary1337/url-shortener/internal/app/entity"
 	"github.com/leary1337/url-shortener/internal/app/service"
 	"github.com/leary1337/url-shortener/pkg/logger"
 )
@@ -28,6 +30,7 @@ func NewShortenerHandler(l logger.Interface, service service.Shortener, redirect
 func (h *ShortenerHandler) RegisterRoutes(r chi.Router) {
 	r.Post("/", h.ShortenURL)
 	r.Get("/{shortURL}", h.ResolveURL)
+	r.Post("/api/shorten", h.ShortenURLJSON)
 }
 
 func (h *ShortenerHandler) ShortenURL(w http.ResponseWriter, r *http.Request) {
@@ -41,6 +44,37 @@ func (h *ShortenerHandler) ShortenURL(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(http.StatusCreated)
 	_, err = w.Write([]byte(fmt.Sprintf("%s/%s", h.redirectAddr, shortURL)))
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+}
+
+func (h *ShortenerHandler) ShortenURLJSON(w http.ResponseWriter, r *http.Request) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	var srb entity.ShortenRequestBody
+	err = json.Unmarshal(body, &srb)
+	if err != nil || srb.Url == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	shortURL := fmt.Sprintf("%s/%s", h.redirectAddr, h.service.ShortenURL(srb.Url))
+
+	responseBody, err := json.Marshal(&entity.ShortenResponseBody{Result: shortURL})
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	_, err = w.Write(responseBody)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
